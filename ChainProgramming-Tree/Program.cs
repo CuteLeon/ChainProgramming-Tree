6841facde1ae19f1c16c5ef4220978065cf76f2c
Leon <leon.id@qq.com>
2020-03-08 00:31:21 +0800

    modified;

--- ChainProgramming-Tree/Program.cs
+++ ChainProgramming-Tree/Program.cs
@@ -2,11 +2,38 @@
 
 namespace ChainProgramming_Tree
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        static int maxDepth = 0;
+
+        private static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            var root = new TreeNode("root").AddChildren(
+                    ((TreeNode)"A_0").AddChildren(
+                        ((TreeNode)"B_0").AddChildren(
+                            "C_0",
+                            "C_1",
+                            "C_2"),
+                        ((TreeNode)"B_1").AddChildren(
+                            ((TreeNode)"C_3").AddChildren(
+                                "D_0",
+                                ((TreeNode)"D_1").AddChildren(
+                                    "E_0",
+                                    "E_1"
+                                    )))),
+                    "A_1");
+
+            PrintTree(root, 0);
+            Console.WriteLine($"最大深度：{maxDepth}");
+            Console.Read();
+        }
+
+        private static void PrintTree(TreeNode node, int level)
+        {
+            maxDepth = Math.Max(level, maxDepth);
+            Console.WriteLine($"{string.Empty.PadLeft(level, '·')}└ {node.Name}");
+            node.Children.ForEach(child => PrintTree(child, level + 1));
         }
     }
 }
---
+++ ChainProgramming-Tree/TreeNode.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+
+namespace ChainProgramming_Tree
+{
+    internal class TreeNode
+    {
+        public TreeNode(string name)
+        {
+            this.Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public TreeNode Parent { get; set; }
+
+        public List<TreeNode> Children { get; set; } = new List<TreeNode>();
+
+        public TreeNode this[int index] => this.Children[index];
+
+        public static implicit operator TreeNode(string name)
+        {
+            return new TreeNode(name);
+        }
+
+        public TreeNode AddChildren(params TreeNode[] treeNodes)
+        {
+            this.Children.AddRange(treeNodes);
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return this.Name;
+        }
+    }
+}
